a7352231f0b33f04ec926ad4aadf995b2ca8d79c
Adrian <adrian@hiddenltd.com>
2017-02-21 17:17:07 +0000

    Move location api code to LocationAwareActivity

--- app/src/main/java/com/adrianlesniak/beautifulthailand/screens/shared/LocationAwareActivity.java
+++ app/src/main/java/com/adrianlesniak/beautifulthailand/screens/shared/LocationAwareActivity.java
@@ -1,39 +1,84 @@
 package com.adrianlesniak.beautifulthailand.screens.shared;
 
 import android.Manifest;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 
-import com.adrianlesniak.beautifulthailand.utilities.LocationListenerAdapter;
 import com.adrianlesniak.beautifulthailand.cache.LocationCache;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 
 /**
  * Created by adrian on 14/02/2017.
  */
 
-public class LocationAwareActivity extends AppCompatActivity {
+public class LocationAwareActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, ResultCallback<LocationSettingsResult>, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
-    private static final int BT_PERMISSION_REQUEST_FINE_LOCATION = 1;
+    private static final int BT_PERMISSION_REQUEST_FINE_LOCATION = 0x1;
 
-    private LocationManager mLocationManager;
+    public static final int REQUEST_CHECK_SETTINGS = 0x2;
+
+    private GoogleApiClient mGoogleApiClient;
+
+    private LocationRequest mLocationRequest;
+
+    private LocationSettingsRequest.Builder mLocationSettingRequestBuilder;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        this.mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        this.mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        this.mLocationRequest = new LocationRequest()
+                .setNumUpdates(1)
+                .setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+        this.mLocationSettingRequestBuilder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(this.mLocationRequest);
 
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
+    protected void onStart() {
+        super.onStart();
+        this.mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        this.mGoogleApiClient.disconnect();
+    }
+
+    public void requestCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, BT_PERMISSION_REQUEST_FINE_LOCATION);
+            return;
+        }
+
+        PendingResult<LocationSettingsResult> result = LocationServices.SettingsApi.checkLocationSettings(this.mGoogleApiClient, this.mLocationSettingRequestBuilder.build());
+        result.setResultCallback(this);
     }
 
     @Override
@@ -57,20 +102,71 @@ public class LocationAwareActivity extends AppCompatActivity {
             }
         }
     }
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
 
-    public void requestCurrentLocation() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, BT_PERMISSION_REQUEST_FINE_LOCATION);
+    }
 
-            return;
-        }
-        this.mLocationManager.requestSingleUpdate(LocationManager.GPS_PROVIDER, new LocationListenerAdapter() {
-            @Override
-            public void onLocationChanged(Location location) {
+    @Override
+    public void onConnectionSuspended(int i) {
 
-                LocationCache.getInstance().setLocationCache(location);
-                mLocationManager.removeUpdates(this);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onResult(@NonNull LocationSettingsResult locationSettingsResult) {
+
+        Status resultStatus = locationSettingsResult.getStatus();
+
+        switch (resultStatus.getStatusCode()) {
+            case LocationSettingsStatusCodes.SUCCESS: {
+                startLocationUpdates();
+                break;
             }
-        }, null);
+            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED: {
+                try {
+                    resultStatus.startResolutionForResult(this, REQUEST_CHECK_SETTINGS);
+                } catch (IntentSender.SendIntentException e) {
+                    e.printStackTrace();
+                }
+                break;
+            }
+            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE: {
+
+                break;
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQUEST_CHECK_SETTINGS) {
+
+            switch (resultCode) {
+                case RESULT_OK: {
+                    startLocationUpdates();
+                    break;
+                }
+                case RESULT_CANCELED: {
+                    break;
+                }
+            }
+
+        }
+    }
+
+    private void startLocationUpdates() {
+        LocationServices.FusedLocationApi.requestLocationUpdates(this.mGoogleApiClient, this.mLocationRequest, this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        LocationCache.getInstance().setLocationCache(location);
     }
 }
